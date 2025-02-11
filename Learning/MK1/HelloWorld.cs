d8079b4bc3baf4093a6b6e07356b3336deb788f2
Борис Новиков <borya_novikov@hotmail.com>
2025-02-11 19:36:34 +0300

    First Lesson Console

---
+++ Learning/MK1/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine("Wake up, Neo...");
+await Task.Delay(1000);
+Console.Clear();
+
+
+Console.WriteLine("The Matrix has you");
+await Task.Delay(1000);
+Console.Clear();
+
+Console.WriteLine("Follow the white rabbit");
+await Task.Delay(1000);
+Console.Clear();
+
+Console.WriteLine("Knock, kmock, Neo");
+Console.ReadLine();
