fbb2904de75b558b585e4c170bd14044d4118fc0
Борис Новиков <borya_novikov@hotmail.com>
2025-02-12 11:42:07 +0300

    Lesson Collection array, List and enum

---
+++ Learning.Collection/Learning.Collection/Fruits.cs
@@ -0,0 +1,32 @@
+﻿
+/*
+Вывод данных для одномерного массива.
+
+int[] array = new int[5];
+array[0] = 15;
+array[1] = 10;
+array[2] = 20;
+array[3] = 30;
+array[4] = 44;
+Console.WriteLine(string.Join(", ", array));
+*/
+
+/*
+ Вывод данных из списка.
+
+List<int> list = new List<int>();
+list.Add(10);
+list.Add(14);
+list.Add(24);
+Console.WriteLine(string.Join(", ", list));
+*/
+
+enum Fruits
+{
+    Apple,
+    Cherry,
+    Orange,
+    Lemon,
+    Avokado,
+    Watermelon
+}
