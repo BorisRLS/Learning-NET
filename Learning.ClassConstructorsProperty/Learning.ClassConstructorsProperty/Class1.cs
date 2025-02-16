c4f54519455da63e98bb8c30aa47665eecfe5425
Борис Новиков <borya_novikov@hotmail.com>
2025-02-16 18:10:47 +0300

    Learning Class, Constructor and Property

---
+++ Learning.ClassConstructorsProperty/Learning.ClassConstructorsProperty/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.ClassConstructorsProperty
+{
+    public class SportFitShopUser
+    {
+        private string fName;
+        private string lName;
+        public string FirstName
+        {
+            get
+            {
+                return fName;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(fName))
+                {
+                    throw new ArgumentNullException("Имя не может быть пустым");
+                }
+                fName = value;
+            }
+        }
+        public string LastName
+       {
+            get
+            {
+                return lName;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(lName))
+                {
+                    throw new ArgumentNullException("Фамилия не может быть пустой");
+                }
+                lName = value;
+            }
+        }
+        public SportFitShopUser(string LastName, string FirstName)
+        {
+            lName = LastName;
+            fName = FirstName;
+        }
+
+        public int SportId { get; set; }
+        public int CardBalance { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+        public string FullSportName
+        {
+            get
+            {
+                return $"{LastName} {FirstName} {SportId} {CardBalance}";
+            }
+        }
+
+    }
+}
---
+++ Learning.ClassConstructorsProperty/Learning.ClassConstructorsProperty/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Learning.ClassConstructorsProperty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Kaminskogo24 = new List<string>();
+
+            var UserList = 0;
+            while (UserList < 1)
+            {
+                UserList++;
+                SportFitShopUser user = new SportFitShopUser(Console.ReadLine(), Console.ReadLine());
+                user.SportId = UserList;
+                user.CardBalance = 0;
+                Console.WriteLine(user.FullSportName);
+                Kaminskogo24.Add(user.FullSportName);
+            }
+        }
+    }
+}
