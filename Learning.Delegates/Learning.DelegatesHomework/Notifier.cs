b4d27af1d9ef1430d92396a1b10d0e4cc3f4c4b0
Борис Новиков <borya_novikov@hotmail.com>
2025-03-11 10:02:09 +0300

    Delegates and Events homework

---
+++ Learning.Delegates/Learning.DelegatesHomework/Notifier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.DelegatesHomework
+{
+    public class Notifier
+    {
+        public delegate void NotificationHandler(string message);
+        public NotificationHandler? OnNotify;
+        public event NotificationHandler? NotifyEvent;
+
+        public void TriggerEvent(string message)
+        {
+            if (StringLength(message) > 5)
+                NotifyEvent?.Invoke(message);
+        }
+        public void SendNotification(string message)
+        {
+            OnNotify?.Invoke(message);
+        }
+        public static int StringLength(string input)
+        {
+            return input.Length;
+        }
+    }
+}
---
+++ Learning.Delegates/Learning.DelegatesHomework/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+
+using Learning.DelegatesHomework;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Notifier notifier = new Notifier();
+
+        // Подписываем метод Console.WriteLine на делегат
+        notifier.OnNotify = Console.WriteLine;
+
+        // Можно добавить ещё один обработчик
+        notifier.OnNotify += PrintMessage;
+
+        // Отправляем уведомление
+        notifier.SendNotification("Привет, это тестовое уведомление!");
+
+        // Создаём слушателей
+        Listener listener1 = new Listener();
+        Listener listener2 = new Listener();
+        Listener listener3 = new Listener();
+
+        // Подписываем слушателей на NotifyEvent
+        notifier.NotifyEvent += listener1.OnNotificationReceived;
+        notifier.NotifyEvent += listener2.OnNotificationReceived;
+        notifier.NotifyEvent += listener3.OnNotificationReceived;
+
+        Console.WriteLine("\n--- Запуск тестового события ---");
+        notifier.TriggerEvent("Запуск тестового события");
+
+        // Убираем второго и третьего подписчика
+        notifier.NotifyEvent -= listener2.OnNotificationReceived;
+        notifier.NotifyEvent -= listener3.OnNotificationReceived;
+
+        Console.WriteLine("\n--- Событие после отписки некоторых слушателей ---");
+        notifier.TriggerEvent("Запуск тестового события");
+
+        Console.WriteLine("\n--- Последнее тестовое событие ---");
+        notifier.TriggerEvent("Пуск");
+
+        Console.WriteLine("\nГотово!");
+    }
+
+    public static void PrintMessage(string message)
+    {
+        Console.WriteLine($"[PrintMessage]: {message}");
+    }
+}
+
